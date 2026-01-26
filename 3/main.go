5e31ab5015797453275d72e7f9632d01c569d3c9
doppie <aegiruchan@gmail.com>
2026-01-26 23:59:21 +0100

    day 3 - second star

--- 3/main.go
+++ 3/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math"
 	"os"
+	"strconv"
 
-	"doppie.com/aoc-common"
+	common "doppie.com/aoc-common"
 )
 
 func findBestBank(s string) int {
@@ -26,6 +29,43 @@ func findBestBank(s string) int {
     return best
 }
 
+func makeKey(s string, i int) string {
+    return s + "," + strconv.Itoa(i)
+}
+
+func findBestDigits(s string, maxLength int) int {
+    memo := make(map[string]int)
+
+    var best func(s string, digits int) int
+    best = func(s string, digits int) int {
+        key := makeKey(s, digits)
+        if cached, ok := memo[key]; ok {
+            return cached
+        }
+        if digits == 0 {
+            return 0
+        }
+
+        if len(s) == digits {
+            v, err := strconv.Atoi(s)
+            if err != nil {
+                log.Fatal("Conversion failed!")
+            }
+            return v
+        }
+
+        a := int(s[0]-'0')*int(math.Pow(10, float64(digits-1))) + best(s[1:], digits-1)
+        b := best(s[1:], digits)
+        v := max(a, b)
+
+        memo[key] = v
+        return v
+    }
+    
+    return best(s, maxLength)
+}
+
+
 func findMaxJoltage(c []string) int {
     sum := 0
 
@@ -39,6 +79,18 @@ func findMaxJoltage(c []string) int {
     return sum
 }
 
+func findLargeJoltage(c []string) int {
+    sum := 0
+
+    for _, v := range c {
+        set := findBestDigits(v, 12)
+        fmt.Printf("Found set: %d.\n", set)
+        sum += set
+    }
+
+    return sum
+}
+
 func main() {
     argsWithoutProg := os.Args[1:]
     f := argsWithoutProg[0]
@@ -47,5 +99,9 @@ func main() {
 
     j := findMaxJoltage(contents)
 
-    fmt.Printf("Found max joltage: %d.", j)
+    fmt.Printf("Found max joltage: %d.\n", j)
+
+    newJ := findLargeJoltage(contents[1:])
+
+    fmt.Printf("Found max new joltage: %d.\n", newJ)
 }
