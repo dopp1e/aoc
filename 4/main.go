8ff2ebb58bac9c069ca8e5a721004ecad29bb0b3
doppie <aegiruchan@gmail.com>
2026-01-27 00:26:35 +0100

    day 4 - second star

--- 4/main.go
+++ 4/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"unicode/utf8"
 
-	"doppie.com/aoc-common"
+	common "doppie.com/aoc-common"
 )
 
 func getGridSize(c []string) (int, int) {
@@ -15,7 +15,6 @@ func getGridSize(c []string) (int, int) {
 
     y := len(c)
     x := utf8.RuneCountInString(c[1])
-    fmt.Printf("(%d, %d)", x, y)
     return x, y
 }
 
@@ -41,8 +40,6 @@ func canBeAccessed(contents []string, x, y, sizeX, sizeY int) bool {
         }
     }
 
-    fmt.Printf("At (%d, %d) found %d neighbors.\n", x, y, neighbors)
-
     return neighbors < 4
 }
 
@@ -66,6 +63,65 @@ func findPaperRolls(contents []string) int {
     return sum
 }
 
+
+func canBeAccessedGrid(grid [][]byte, x, y, sizeX, sizeY int) bool {
+    neighbors := 0
+
+    for i := -1; i <= 1; i++ {
+        for j := -1; j <= 1; j++ {
+            cx := x + i
+            cy := y + j
+            if cx == x && cy == y {
+                continue
+            }
+            if areCoordsWithinBounds(cx, cy, sizeX, sizeY) {
+                if grid[cy][cx] == '@' {
+                    neighbors++
+                }
+            }
+        }
+    }
+
+    return neighbors < 4
+}
+
+func findAndRemovePaperRolls(contents []string) int {
+    sum := 0
+
+    sizeX, sizeY := getGridSize(contents)
+    if sizeX == 0 || sizeY == 0 {
+        return 0
+    }
+
+    // Convert to [][]byte for easier modification
+    grid := make([][]byte, sizeY)
+    for i := range contents {
+        grid[i] = []byte(contents[i])
+    }
+
+    changed := true
+    for changed {
+        changed = false
+        coords := make([][2]int, 0)
+
+        for i := range sizeX {
+            for j := range sizeY {
+                if grid[j][i] == '@' && canBeAccessedGrid(grid, i, j, sizeX, sizeY) {
+                    sum++
+                    coords = append(coords, [2]int{i, j})
+                    changed = true
+                }
+            }
+        }
+
+        for _, v := range coords {
+            grid[v[1]][v[0]] = 'x'
+        }
+    }
+
+    return sum
+}
+
 func main() {
     argsWithoutProg := os.Args[1:]
     f := argsWithoutProg[0]
@@ -75,4 +131,8 @@ func main() {
     rolls := findPaperRolls(contents)
 
     fmt.Printf("A forklift can access %d paper rolls.\n", rolls)
+
+    moreRolls := findAndRemovePaperRolls(contents)
+
+    fmt.Printf("A forklift can access and remove %d paper rolls.\n", moreRolls)
 }
