4522439bef3fcbfe486c0d182c1e600b50fa8b93
doppie <aegiruchan@gmail.com>
2026-01-29 00:02:32 +0100

    day 5 - second star

--- 5/main.go
+++ 5/main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 
 	common "doppie.com/aoc-common"
 )
+
+type SortBy [][2]int
+	
+func (a SortBy) Len() int           { return len(a) }
+func (a SortBy) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a SortBy) Less(i, j int) bool {
+	if a[i][0] == a[j][0] {
+		return a[i][1] < a[j][1]
+	} else {
+		return a[i][0] < a[j][0]
+	}
+}
 
 func isFresh(a int, rangeArr [][2]int) bool {
 	for i := range len(rangeArr) {
@@ -19,11 +32,28 @@ func isFresh(a int, rangeArr [][2]int) bool {
 	return false
 }
 
-func countSpoiledIngredients(c []string) int {
+func countAllIDs(rangeArr [][2]int) int {
+	sort.Sort(SortBy(rangeArr))
+
+	count := 0
+	prevEnd := 0
+	for i := range len(rangeArr) {
+		start := rangeArr[i][0]
+		end := rangeArr[i][1] + 1
+
+		if end > prevEnd {
+			count += end - max(start, prevEnd)
+			prevEnd = end
+		}
+	}
+
+	return count
+}
+
+func countSpoiledIngredients(c []string) (int, [][2]int) {
 	sum := 0
 	checkMode := false
 	rangeArr := make([][2]int, 0)
-
 
 	for _, l := range c {
 		if l == "" {
@@ -53,7 +83,7 @@ func countSpoiledIngredients(c []string) int {
 		}
 	}
 
-	return sum
+	return sum, rangeArr
 }
 
 func main() {
@@ -62,7 +92,11 @@ func main() {
 
     contents := common.ReadFile(f)[1:]
 
-	sum := countSpoiledIngredients(contents)
+	sum, r := countSpoiledIngredients(contents)
 
 	log.Printf("%d ingredients are fresh.", sum)
+
+	all := countAllIDs(r)
+
+	log.Printf("%d IDs have been declared.", all)
 }
