c17d438e4fb37e8ab41c9b20a5e129a8dc974c5e
doppie <aegiruchan@gmail.com>
2026-02-02 22:27:48 +0100

    day 7 - first star

--- 7/main.go
+++ 7/main.go
@@ -45,6 +45,56 @@ func splitBeams(c []string) int {
 	return splitCount
 }
 
+
+
+func startQuantum(c []string) int {
+	grid := make([][]byte, len(c))
+    for i := range c {
+        grid[i] = []byte(c[i])
+    }
+
+	sCol := 0
+
+	for i := 0; i < len(c[0]); i++ {
+		if c[0][i] == 'S' {
+			sCol = i
+		}
+	}
+
+	memo := make(map[[2]int]int)
+
+	var quantumSplitBeams func(g [][]byte, row, col int) int
+	quantumSplitBeams = func(g [][]byte, row, col int) int {
+		val, ok := memo[[2]int{row, col}]
+		if ok {
+			return val
+		}
+		sum := 0
+
+		l := len(g)
+
+		notBroken := true
+
+		for i := row; i < l; i++ {
+			if (g[i][col] == '^') {
+				sum += quantumSplitBeams(g, i, col - 1) + quantumSplitBeams(g, i, col + 1)
+				notBroken = false
+				break
+			}
+		}
+
+		if notBroken {
+			sum += 1
+		}
+
+		memo[[2]int{row, col}] = sum
+
+		return sum
+	}
+
+	return quantumSplitBeams(grid, 1, sCol)
+}
+
 func main() {
     argsWithoutProg := os.Args[1:]
     f := argsWithoutProg[0]
@@ -53,6 +103,9 @@ func main() {
 
 	c := splitBeams(contents)
 
-	fmt.Printf("The beam has been split %d times!", c)
+	fmt.Printf("The beam has been split %d times!\n", c)
 
+	q := startQuantum(contents)
+
+	fmt.Printf("The beam has been split into %d timelines!\n", q)
 }
