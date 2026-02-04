2226afe64d6262b5f5885bbb4233e55dab89050d
doppie <aegiruchan@gmail.com>
2026-02-04 16:40:17 +0100

    day 8 - first star

---
+++ 8/main.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
+
+	common "doppie.com/aoc-common"
+)
+
+// type for keeping distances between junction boxes
+type distance struct {
+	s1 string
+	s2 string
+	dist float64
+}
+
+// helper type for sorting distances
+type DistanceSort []distance
+
+func (a DistanceSort) Len() int           { return len(a) }
+func (a DistanceSort) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a DistanceSort) Less(i, j int) bool { return a[i].dist < a[j].dist }
+
+// given string "x,y,z" unpack and turn into ints
+func unpackCoordinates(s string) (int, int, int) {
+	spl := strings.Split(s, ",")
+	x, err := strconv.Atoi(spl[0])
+	if err != nil {
+		log.Fatalf("Failed converting first element of %s to int!\n", s)
+	}
+	y, err := strconv.Atoi(spl[1])
+	if err != nil {
+		log.Fatalf("Failed converting second element of %s to int!\n", s)
+	}
+	z, err := strconv.Atoi(spl[2])
+	if err != nil {
+		log.Fatalf("Failed converting third element of %s to int!\n", s)
+	}
+	return x, y, z
+}
+
+// calculate distance between two sets of x,y,z coordinates
+func euclideanDistance(x1, y1, z1, x2, y2, z2 int) float64 {
+	return math.Sqrt(math.Pow(float64(x2) - float64(x1), 2) + math.Pow(float64(y2) - float64(y1), 2)+ math.Pow(float64(z2) - float64(z1), 2))
+}
+
+func putTogether(s1, s2 string) string {
+	return s1 + ";" + s2
+}
+
+// func isInString(s1, s2 string, check distance) bool {
+// 	firstOrder := s1 == check.s1 && s2 == check.s2
+// 	secondOrder := s1 == check.s2 && s2 == check.s1
+// 	return firstOrder || secondOrder  
+// }
+
+func areAlreadyInList(s1, s2 string, passed map[string]bool) bool {
+	_, ok1 := passed[putTogether(s1, s2)]
+	_, ok2 := passed[putTogether(s2, s1)]
+	return ok1 || ok2
+}
+
+func connectBoxes(c []string, connections int) int {
+	m := 1
+
+	// calculate all distances between points...
+	list := make([]distance, 0)
+	passedCache := make(map[string]bool)
+	
+	for i := 0; i < len(c); i++ {
+		x1, y1, z1 := unpackCoordinates(c[i])
+		for j := 0; j < len(c); j++ {
+			// ...except between the same points
+			if i == j {
+				continue
+			}
+			// if already calculated, skip
+			if areAlreadyInList(c[i], c[j], passedCache) {
+				continue
+			}
+
+			x2, y2, z2 := unpackCoordinates(c[j])
+			dist := euclideanDistance(x1, y1, z1, x2, y2, z2)
+			list = append(list, distance{s1: c[i], s2: c[j], dist: dist})
+			passedCache[putTogether(c[i], c[j])] = true
+		}
+	}
+
+	// we have a list of distances between junction boxes sorted by distance
+	sort.Sort(DistanceSort(list))
+
+	// keeping connectionIDs
+	connectionMap := make(map[string]int)
+	currentIndex := 0
+
+	for i := 0; connections > 0; i++ {
+		if i >= len(list) {
+			break
+		}
+		cur := list[i]
+		// if both have already been assigned... continue???
+		c1, ok1 := connectionMap[cur.s1]
+		c2, ok2 := connectionMap[cur.s2]
+		if ok1 && ok2 {
+			// replace all IDs in second one with the values of the first one
+			for k := range connectionMap {
+				if connectionMap[k] == c2 {
+					connectionMap[k] = c1
+				}
+			}
+			connections--
+			continue
+		}
+		// if only one has been connected
+		if ok1 != ok2 {
+			// add whichever one is not connected to where the other one is connected
+			if ok1 {
+				connectionMap[cur.s2] = c1
+				//fmt.Printf("Adding %s to %d.\n", cur.s2, c1)
+			} else if ok2 {
+				connectionMap[cur.s1] = c2
+				//fmt.Printf("Adding %s to %d.\n", cur.s1, c2)
+			}
+			connections--
+			continue
+		}
+		// if neither is connected
+		// give them indices
+		connectionMap[cur.s1] = currentIndex
+		connectionMap[cur.s2] = currentIndex
+		currentIndex++
+		//fmt.Printf("New connection between %s and %s made.\n", cur.s1, cur.s2)
+		connections--
+	}
+
+	// count instances of numbers
+	countMap := make(map[int]int)
+
+	for k := range connectionMap {
+		_, ok := countMap[connectionMap[k]]
+		if ok {
+			countMap[connectionMap[k]]++
+		} else {
+			countMap[connectionMap[k]] = 1
+		}
+	}
+
+	allConnections := make([]int, 0)
+
+	for k := range countMap {
+		//fmt.Printf("%d = %d\n", k, countMap[k])
+		allConnections = append(allConnections, countMap[k])
+	}
+
+	slices.Sort(allConnections)
+
+	for i := 0; i < min(3, len(allConnections)); i++ {
+		id := len(allConnections) - 1 - i
+		//fmt.Printf("Multiplying %d by %d.\n", m, allConnections[id])
+		m *= allConnections[id]
+	}
+
+	return m
+}
+
+func main() {
+    argsWithoutProg := os.Args[1:]
+    f := argsWithoutProg[0]
+
+    contents := common.ReadFile(f)[1:]
+
+	c := connectBoxes(contents, 10)
+
+	fmt.Printf("The size of the circuit with 10 connections equals %d!\n", c)
+
+	d := connectBoxes(contents, 1000)
+
+	fmt.Printf("The size of the circuit with 1000 connections equals %d!\n", d)
+}
